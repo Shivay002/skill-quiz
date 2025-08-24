fff2ee42df2e3517fcf1836909329b6e3bf13b49
Shivay002 <sshivams002@gmail.com>
2025-08-24 16:17:06 +0530

    minor change

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,13 +2,6 @@ import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 
 dotenv.config();
-console.log(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  process.env.DB_HOST,
-  process.env.DB_PORT,'DB-COFIG->>>>>>>>>>>>>>'
-);
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
--- skill-portal-frontend/src/api/axiosInstance.js
+++ skill-portal-frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_REACT_APP_API_URL}/api`,
 });
 
 axiosInstance.interceptors.request.use((config) => {
